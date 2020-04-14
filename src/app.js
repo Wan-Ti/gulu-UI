2c62ddc0999292db1ad938b96c80e0a6ac9a87ab
Wan-Ti <1826155752@qq.com>
2020-04-14 19:49:32 +0800

    button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import Vue from 'vue'
 import Button from './button'
+import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
+
+import createElement from 'vue'
+
+const h = createElement
 
 new Vue({
     el: '#app',
+    data: {
+        loading1: false,
+        loading2: true,
+        loading3: false,
+        message: 'hi',
+        selectedTab: 'sports'
+    },
+    created() {
+    },
 })
-
-
-
--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,40 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+    <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+    <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-export default {};
+export default {
+  name: "GuluButton",
+  props: {
+    icon: {},
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        return value === "left" || value === "right";
+      }
+    }
+  }
+};
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
@@ -12,6 +42,10 @@ export default {};
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -21,5 +55,25 @@ export default {};
   &:focus {
     outline: none;
   }
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+  &.icon-right {
+    > .content {
+      order: 1;
+    }
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.3em;
+    }
+  }
+  .loading {
+    animation: spin 2s infinite linear;
+  }
 }
-</style> 
+</style>
